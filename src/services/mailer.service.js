53f75937997a62a351791d5e62f6c0cfda959012
Hadiur <fmhadiuzzaman@gmail.com>
2025-10-30 22:28:03 +0100

    added mail SMTP_PORT and SMTP_HOST

--- src/services/mailer.service.js
+++ src/services/mailer.service.js
@@ -53,7 +53,7 @@ export async function sendMail({ to, subject, html, text, attachments } = {}) {
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
-      pass: (process.env.EMAIL_PASS || '').replace(/\s+/g, ''), // strip spaces
+      pass: process.env.EMAIL_PASS, // strip spaces
     },
   });
 
